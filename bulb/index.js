384ed9e0d7daadde41b92548d45edf5285ce4520
Osaherume98 <130377941+Osaherume98@users.noreply.github.com>
2023-09-27 05:19:53 +0100

    Add files via upload

---
+++ bulb/index.js
@@ -0,0 +1,7 @@
+function bulb_off() {
+  document.getElementById("light").src = "image/download_off.jpeg";
+}
+
+function bulb_on() {
+  document.getElementById("light").src = "image/download_on.jpeg";
+}
